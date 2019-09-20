6badfaf9d0eb41e835f08507643b1eb0df6ad5e0
LostMystic <mark@lostmystic.com>
2019-09-20 00:35:47 -0600

    Locations window base code.

--- Scripts/Editor/EditorDfLocationsWindow.cs
+++ Scripts/Editor/EditorDfLocationsWindow.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop;
 using DaggerfallWorkshop.DungeonGenerator;
+using DaggerfallConnect;
+using DaggerfallConnect.Arena2;
 
 public class EditorDfLocationsWindow : EditorWindow
 {
@@ -77,20 +81,20 @@ public class EditorDfLocationsWindow : EditorWindow
         // Map Tile Location, or Interior or Dungeon  Tabs
 
         // Two options: Select then Replace/Insert
-            // This zooms map to locations or blocks and allows user to replace or add entry to a location
+        // This zooms map to locations or blocks and allows user to replace or add entry to a location
 
 
         // Dungeon Layout
-            // Dungeon Block
-                // Door
-                // Model / Flat / Event
+        // Dungeon Block
+        // Door
+        // Model / Flat / Event
 
         // Option 2: Import Location - Bring up the area first, then pick where to insert it
         // If Dungeon
         // Replace existing location or add new location
         // Add new location loads a dungeon layout, must have exit(s) the lead to location(s), but with 
         // If add new location needs an above ground entrance with a map tile or keyed door
-            // Verifies all features that need to be in area.  Modifies prefab
+        // Verifies all features that need to be in area.  Modifies prefab
 
         // Mod adds interruption in between specific location doors or when streaming a tile
 
@@ -100,7 +104,7 @@ public class EditorDfLocationsWindow : EditorWindow
         // Override existing place (with mod)
         // key a specific door
         // Scripted command (Dialog, falling down a pit, or if you have another area that will reference it later)
-            // Key to a generated location
+        // Key to a generated location
         // backup area to send person to if it doesn't work.
 
         //DFLocation
@@ -109,16 +113,48 @@ public class EditorDfLocationsWindow : EditorWindow
         // May be in DaggerFall unity during runtime
         // Playerenterexit has a location override
         // Dungeon --> DaggerfallDungeon  monobehaviour
-            // chidren have list of dungeon blocks and enemies
-            // has location textures
-            // When dungeon is gone it is removed from the dungeon branch
+        // chidren have list of dungeon blocks and enemies
+        // has location textures
+        // When dungeon is gone it is removed from the dungeon branch
         // Outdoor locations have list of doors, which are automaticlaly mapped onto the terrain
         // DaggerfallStaticDoors
 
-        if (GUILayout.Button("Do thing", GUILayout.Width(buttonWidth), GUILayout.Height(buttonHeight)))
+        // Location contains a dungeon
+        // Has a block index in the static doors list - Block index in BLOCKS.BSA
+        // Record index is for a specific interior (like shop or home)
+        // each location(site) is marked on the world map.  Each site can have a number of exterior blocks, and
+        // a dungeon attached to it.
+        // Everything for transport is in player enter exit
+        // DFLocation.RegionMapTable
+        //locationdungeon
+        // Has array of dungeonblock
+        // !! Must overwrite a normal dungeon block with a more base form.
+        // That must simulate any features a normal dungeon would have for compatibility.
+        // LocationIndex?
+
+        // ?? Where is the index of locations you can intercept for mods? Based on GPS?
+        // Can you intercept a transition request event?
+        // Requests to Maps.bsa
+        // DFValidator:152  // Supports alternate MAPS.BSA from Resources if available
+
+        if (GUILayout.Button("Teleport Player to test dungeon area", GUILayout.Width(buttonWidth), GUILayout.Height(buttonHeight)))
         {
-            
+            // Test if game is running
+            if (IsGameRunning() == false) return;
+
+            Transform doorOwner = null;
+            StaticDoor door = new StaticDoor();
+            DFLocation location = new DFLocation();
+
+            // Assign an override location
+
+            // Grab player and use their PlayerEnterExit
+            // TransitionDungeonInterior
+            GameManager.Instance.PlayerEnterExit.TransitionDungeonInterior(doorOwner, door, location);
+
         }
+
+
 
         if (GUILayout.Button("Do other thing", GUILayout.Width(buttonWidth), GUILayout.Height(buttonHeight)))
         {
@@ -126,12 +162,68 @@ public class EditorDfLocationsWindow : EditorWindow
 
         }
 
+        PrintLocationInformation();
+
         EditorGUILayout.EndHorizontal();
 
     }
 
     #endregion
 
+    bool IsPrefab(GameObject gameObject)
+    {
+        if (gameObject == null) return false;
+
+        if (PrefabUtility.GetPrefabType(gameObject) == PrefabType.Prefab) return true;
+
+        return false;
+    }
+
+    bool IsGameRunning()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+        if (go == null) return false;
+
+        DaggerfallWorkshop.PlayerGPS gPS = go.GetComponent<DaggerfallWorkshop.PlayerGPS>();
+
+
+        if (gPS == null) return false;
+
+        return true;
+    }
+
+    GameObject DungeonPrefab;
+    string LocationName;
+    int RegionSelected = 0;
+
+    void PrintLocationInformation()
+    {
+        int[] RegionValues = {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44
+        };
+
+        GUILayout.BeginVertical();
+
+        DungeonPrefab = EditorGUILayout.ObjectField("", DungeonPrefab, typeof(GameObject)) as GameObject;
+
+        if (IsPrefab(DungeonPrefab) == false) DungeonPrefab = null;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Location Name:");
+        LocationName = GUILayout.TextField(LocationName, GUILayout.MinWidth(80));
+        GUILayout.EndHorizontal();
+
+        
+
+        RegionSelected = EditorGUILayout.IntPopup("Region",RegionSelected, MapsFile.RegionNames, RegionValues,GUILayout.MinWidth(80));
+
+        // Coordinates on map (with popup map)
+        GUILayout.Label("Map Coords:");
+
+    }
 
     void Update()
     {
